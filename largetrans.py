bbe735dabf4301bd8a57983d6ade4532a9b04a5a
zeclixx <zeclix1013@gmail.com>
2015-05-23 03:09:19 +0900

    add files

---
+++ largetrans.py
@@ -0,0 +1,24 @@
+import os
+
+for root, dirs, files in os.walk('.' + os.path.sep):
+	for filename in files:  
+		ext = os.path.splitext(filename)[-1]
+		name = os.path.splitext(filename)[0]
+		if ext == '.png-large':
+			try:
+				os.rename(root + os.path.sep + filename, root + os.path.sep + name+'.png')
+
+			except WindowsError as error:
+				error = str(error)
+				if error.find('183') > 0:
+					os.remove(root + os.path.sep + name+'.png')
+					os.rename(root + os.path.sep + filename, root + os.path.sep + name+'.png')
+		elif ext== '.jpg-large':
+			try:
+				os.rename(root + os.path.sep + filename, root + os.path.sep + name+'.jpg')
+
+			except WindowsError as error:
+				error = str(error)
+				if error.find('183') > 0:
+					os.remove(root + os.path.sep + name+'.jpg')
+					os.rename(root + os.path.sep + filename, root + os.path.sep + name+'.jpg')
